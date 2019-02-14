95eea5607f1aa58a16b1708457550d84d42bfa56
Mark Warfield <mark.warfield@dreamm.co.uk>
2019-02-14 18:22:34 +0000

    initial commit tweaks

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,19 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      marginBottom: `1.45rem`
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.45rem 1.0875rem`
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -21,22 +21,40 @@ const Header = ({ siteTitle }) => (
           to="/"
           style={{
             color: `white`,
-            textDecoration: `none`,
+            textDecoration: `none`
           }}
         >
           {siteTitle}
         </Link>
+        <Link
+          to="/about"
+          style={{
+            color: `white`,
+            textDecoration: `none`
+          }}
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+          style={{
+            color: `white`,
+            textDecoration: `none`
+          }}
+        >
+          Contact
+        </Link>
       </h1>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: ``
+};
 
-export default Header
+export default Header;
